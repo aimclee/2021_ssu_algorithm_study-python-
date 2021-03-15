56050c997baf4d1529133ce44b6e74c4c65296b1
HyewonJu <79213013+HyewonJu@users.noreply.github.com>
2021-03-15 23:19:39 +0900

    Create 큰수의법칙.py

---
+++ Greedy/큰수의법칙.py
@@ -0,0 +1,28 @@
+# elapsed time :  6.357971906661987
+
+import time
+
+start_time = time.time()
+
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+tmp = K 
+sum = 0 
+
+data.sort()
+first = data[-1]
+second = data[-2]
+
+for i in range(0, M): 
+    if tmp!=0:
+        sum+=first
+    else: 
+        sum+=second
+        tmp=K
+    tmp-=1
+    
+print(sum)
+
+end_time = time.time()
+
+print("elapsed time : ", end_time-start_time)
