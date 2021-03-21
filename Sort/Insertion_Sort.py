d466bb58b78db9ddf8e28eab95c32c9284da79e2
HyewonJu <79213013+HyewonJu@users.noreply.github.com>
2021-03-22 00:55:21 +0900

    Insertion Sort

---
+++ Sort/Insertion_Sort.py
@@ -0,0 +1,13 @@
+# 시간복잡도
+# 최선의 경우 : O(n) - 데이터가 거의 정렬되어 있는 상태, 퀵 정렬보다 강력
+# 평균시간복잡도 : O(n^2)
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j]<array[j-1]: # 한칸씩 왼쪽으로 이동
+            array[j], array[j-1] = array[j-1], array[j]
+        else: # 자기보다 작은 데이터를 만나면 그위치에서 멈춤
+            break
+print(array)
